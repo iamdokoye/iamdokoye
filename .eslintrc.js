2822615a14ca1017f496289a2b0142d5389ffd5f
Daniel Okoye <okoyedann@gmail.com>
2025-08-27 08:18:39 +0100

    Added CI file

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'react-app',
+    'react-app/jest'
+  ],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    // Security-focused rules (no plugins required)
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-script-url': 'error',
+    'no-unsanitized/method': 'error',
+    'no-unsanitized/property': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    
+    // Code quality
+    'prefer-const': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { 
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_' 
+    }]
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx'],
+      rules: {
+        'no-console': 'off'
+      }
+    }
+  ]
+};
